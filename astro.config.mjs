b407b206eace49c7575365fcea07817e97389ebb
mahima-levein <mahima.levein@gmail.com>
2026-05-19 09:12:35 +0530

    Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -19,5 +21,6 @@ export default defineConfig({
     },
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
